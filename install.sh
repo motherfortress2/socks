334510c350a058994679468be3bda4fd1e751978
SpaceWD <121475790+motherfortress2@users.noreply.github.com>
2024-09-29 15:09:24 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+clear
+echo "slippers installation script, work in progress."
+echo "installing slippers"
+git clone https://github.com/motherfortress2/socks
+cd socks
+pip install -r requirements.txt
+echo "finished. if you encountered any errors then you can try to install the requirements later."
+echo "do you want to install the media server?" media
+if [ "$media" == "y" ] || [ "$media" == "Y" ]; then
+  cd htdocs && git clone https://github.com/motherfortress2/2005
+else
+    echo "You chose not to install the media server. You can add the 2007 media server now. If you made a mistake and you want the 2005 media server, copy and execute "cd htdocs && git clone https://github.com/motherfortress2/2005""
+fi
+echo "thanks for trying this installation script. things will only improve from here!"
