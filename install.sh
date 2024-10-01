93a24248a8cb36db1112b3bf7bec65d3ad54caca
SpaceWD <121475790+motherfortress2@users.noreply.github.com>
2024-10-01 09:47:05 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ cd socks
 pip install -r requirements.txt
 echo "finished. if you encountered any errors then you can try to install the requirements later."
 echo "do you want to install the media server?" media
-if [ "$media" == "y" ] || [ "$media" == "Y" ]; then
+if [ "$media" == "yes" ] || [ "$media" == "Yes" ]; then
   cd htdocs && git clone https://github.com/motherfortress2/2005
 else
     echo "You chose not to install the media server. You can add the 2007 media server now. If you made a mistake and you want the 2005 media server, copy and execute "cd htdocs && git clone https://github.com/motherfortress2/2005""
